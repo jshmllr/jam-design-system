89ba23854afbb753557097eca14cdee2f8d1ffb2
Joshua <joshua@jshmllr.com>
2023-01-04 10:37:25 -0600

    adding the tokens!

---
+++ build/ios/StyleDictionarySize.h
@@ -0,0 +1,15 @@
+
+// StyleDictionarySize.h
+//
+
+// Do not edit directly
+// Generated on Wed, 04 Jan 2023 16:37:11 GMT
+
+
+#import <Foundation/Foundation.h>
+
+
+extern float const SizeFontSmall;
+extern float const SizeFontMedium;
+extern float const SizeFontLarge;
+extern float const SizeFontBase;
---
+++ build/ios/StyleDictionarySize.m
@@ -0,0 +1,16 @@
+
+//
+// StyleDictionarySize.m
+//
+
+// Do not edit directly
+// Generated on Wed, 04 Jan 2023 16:37:11 GMT
+
+
+#import "StyleDictionarySize.h"
+
+
+float const SizeFontSmall = 12.00f;
+float const SizeFontMedium = 16.00f;
+float const SizeFontLarge = 32.00f;
+float const SizeFontBase = 16.00f;
