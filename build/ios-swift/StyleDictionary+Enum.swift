78dd63225be60d2a3105920f320eb963c6b756a1
Joshua <joshua@jshmllr.com>
2023-01-04 11:06:04 -0600

    adding back in extra examples based on example styles

---
+++ build/ios-swift/StyleDictionary+Enum.swift
@@ -0,0 +1,77 @@
+
+//
+// StyleDictionary+Enum.swift
+//
+
+// Do not edit directly
+// Generated on Wed, 04 Jan 2023 17:05:28 GMT
+
+
+import UIKit
+
+public enum StyleDictionaryEnum {
+    public static let globalBaseBlack = #000000
+    public static let globalBaseWhite = #ffffff
+    public static let globalError100 = #fee4e2
+    public static let globalError200 = #fecdca
+    public static let globalError25 = #fffbfa
+    public static let globalError300 = #fda29b
+    public static let globalError400 = #f97066
+    public static let globalError50 = #fef3f2
+    public static let globalError500 = #f04438
+    public static let globalError600 = #d92d20
+    public static let globalError700 = #b42318
+    public static let globalError800 = #912018
+    public static let globalError900 = #7a271a
+    public static let globalGradientPrimary60050090deg = linear-gradient(90deg, #7f56d9 0%, #9e77ed 100%)
+    public static let globalGradientPrimary70060045deg = linear-gradient(45deg, #6941c6 0%, #7f56d9 100%)
+    public static let globalGradientPrimary80060045deg = linear-gradient(45deg, #53389e 0%, #7f56d9 100%)
+    public static let globalGradientPrimary80060090deg = linear-gradient(90deg, #53389e 0%, #7f56d9 100%)
+    public static let globalGradientPrimary80070026deg = linear-gradient(27deg, #53389e 0%, #6941c6 100%)
+    public static let globalGradientPrimary90060045deg = linear-gradient(45deg, #42307d 0%, #7f56d9 100%)
+    public static let globalGray100 = #f2f4f7
+    public static let globalGray200 = #eaecf0
+    public static let globalGray25 = #fcfcfd
+    public static let globalGray300 = #d0d5dd
+    public static let globalGray400 = #98a2b3
+    public static let globalGray50 = #f9fafb
+    public static let globalGray500 = #667085
+    public static let globalGray600 = #475467
+    public static let globalGray700 = #344054
+    public static let globalGray800 = #1d2939
+    public static let globalGray900 = #101828
+    public static let globalPrimary100 = #f4ebff
+    public static let globalPrimary200 = #e9d7fe
+    public static let globalPrimary25 = #fcfaff
+    public static let globalPrimary300 = #d6bbfb
+    public static let globalPrimary400 = #b692f6
+    public static let globalPrimary50 = #f9f5ff
+    public static let globalPrimary500 = #9e77ed
+    public static let globalPrimary600 = #7f56d9
+    public static let globalPrimary700 = #6941c6
+    public static let globalPrimary800 = #53389e
+    public static let globalPrimary900 = #42307d
+    public static let globalRoboto = Roboto
+    public static let globalSuccess100 = #d1fadf
+    public static let globalSuccess200 = #a6f4c5
+    public static let globalSuccess25 = #f6fef9
+    public static let globalSuccess300 = #6ce9a6
+    public static let globalSuccess400 = #32d583
+    public static let globalSuccess50 = #ecfdf3
+    public static let globalSuccess500 = #12b76a
+    public static let globalSuccess600 = #039855
+    public static let globalSuccess700 = #027a48
+    public static let globalSuccess800 = #05603a
+    public static let globalSuccess900 = #054f31
+    public static let globalWarning100 = #fef0c7
+    public static let globalWarning200 = #fedf89
+    public static let globalWarning25 = #fffcf5
+    public static let globalWarning300 = #fec84b
+    public static let globalWarning400 = #fdb022
+    public static let globalWarning50 = #fffaeb
+    public static let globalWarning500 = #f79009
+    public static let globalWarning600 = #dc6803
+    public static let globalWarning700 = #b54708
+    public static let globalWarning800 = #93370d
+    public static let globalWarning900 = #7a2e0e
+}
---
+++ build/ios-swift/StyleDictionary+Struct.swift
@@ -0,0 +1,77 @@
+
+//
+// StyleDictionary+Struct.swift
+//
+
+// Do not edit directly
+// Generated on Wed, 04 Jan 2023 17:05:28 GMT
+
+
+import UIKit
+
+internal struct StyleDictionaryStruct {
+    internal static let globalBaseBlack = #000000
+    internal static let globalBaseWhite = #ffffff
+    internal static let globalError100 = #fee4e2
+    internal static let globalError200 = #fecdca
+    internal static let globalError25 = #fffbfa
+    internal static let globalError300 = #fda29b
+    internal static let globalError400 = #f97066
+    internal static let globalError50 = #fef3f2
+    internal static let globalError500 = #f04438
+    internal static let globalError600 = #d92d20
+    internal static let globalError700 = #b42318
+    internal static let globalError800 = #912018
+    internal static let globalError900 = #7a271a
+    internal static let globalGradientPrimary60050090deg = linear-gradient(90deg, #7f56d9 0%, #9e77ed 100%)
+    internal static let globalGradientPrimary70060045deg = linear-gradient(45deg, #6941c6 0%, #7f56d9 100%)
+    internal static let globalGradientPrimary80060045deg = linear-gradient(45deg, #53389e 0%, #7f56d9 100%)
+    internal static let globalGradientPrimary80060090deg = linear-gradient(90deg, #53389e 0%, #7f56d9 100%)
+    internal static let globalGradientPrimary80070026deg = linear-gradient(27deg, #53389e 0%, #6941c6 100%)
+    internal static let globalGradientPrimary90060045deg = linear-gradient(45deg, #42307d 0%, #7f56d9 100%)
+    internal static let globalGray100 = #f2f4f7
+    internal static let globalGray200 = #eaecf0
+    internal static let globalGray25 = #fcfcfd
+    internal static let globalGray300 = #d0d5dd
+    internal static let globalGray400 = #98a2b3
+    internal static let globalGray50 = #f9fafb
+    internal static let globalGray500 = #667085
+    internal static let globalGray600 = #475467
+    internal static let globalGray700 = #344054
+    internal static let globalGray800 = #1d2939
+    internal static let globalGray900 = #101828
+    internal static let globalPrimary100 = #f4ebff
+    internal static let globalPrimary200 = #e9d7fe
+    internal static let globalPrimary25 = #fcfaff
+    internal static let globalPrimary300 = #d6bbfb
+    internal static let globalPrimary400 = #b692f6
+    internal static let globalPrimary50 = #f9f5ff
+    internal static let globalPrimary500 = #9e77ed
+    internal static let globalPrimary600 = #7f56d9
+    internal static let globalPrimary700 = #6941c6
+    internal static let globalPrimary800 = #53389e
+    internal static let globalPrimary900 = #42307d
+    internal static let globalRoboto = Roboto
+    internal static let globalSuccess100 = #d1fadf
+    internal static let globalSuccess200 = #a6f4c5
+    internal static let globalSuccess25 = #f6fef9
+    internal static let globalSuccess300 = #6ce9a6
+    internal static let globalSuccess400 = #32d583
+    internal static let globalSuccess50 = #ecfdf3
+    internal static let globalSuccess500 = #12b76a
+    internal static let globalSuccess600 = #039855
+    internal static let globalSuccess700 = #027a48
+    internal static let globalSuccess800 = #05603a
+    internal static let globalSuccess900 = #054f31
+    internal static let globalWarning100 = #fef0c7
+    internal static let globalWarning200 = #fedf89
+    internal static let globalWarning25 = #fffcf5
+    internal static let globalWarning300 = #fec84b
+    internal static let globalWarning400 = #fdb022
+    internal static let globalWarning50 = #fffaeb
+    internal static let globalWarning500 = #f79009
+    internal static let globalWarning600 = #dc6803
+    internal static let globalWarning700 = #b54708
+    internal static let globalWarning800 = #93370d
+    internal static let globalWarning900 = #7a2e0e
+}
